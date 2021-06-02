3e97caef5b12bf25bd30937ca9c65b93850b99f3
shadykh <eng.shady94@gmail.com>
2021-06-02 16:03:48 +0300

    CodeChallenge 11, v1.3

--- Challenges/queueWithStacks/queue-with-stacks.js
+++ Challenges/queueWithStacks/queue-with-stacks.js
@@ -5,22 +5,49 @@ const StackAndQueue = require('../../Data-Structures/JavaScript/stacksAndQueues/
 class PseudoQueue {
 
     constructor() {
+
         this.firstStack = new StackAndQueue.Stack();
+
         this.secondStack = new StackAndQueue.Stack();
     }
 
-    enqueue(value){
-        if(!value){return '⚠️The passing value is not valid.⚠️'};
-        
+    enqueue(value) {
+
+        if (!value) { return '⚠️The passing value is not valid.⚠️' };
+
         this.firstStack.push(value);
-        return  this.firstStack.top;
+
+        return this.firstStack.top;
     }
 
-    dequeue(){
-        if (this.firstStack.peek() === '⚠️The Stack is empty.⚠️ You can not use the peek ❌'){
+    dequeue() {
+        if (this.firstStack.peek() === '⚠️The Stack is empty.⚠️ You can not use the peek ❌' && this.secondStack.peek() === '⚠️The Stack is empty.⚠️ You can not use the peek ❌') {
+
             return '⚠️The PseudoQueue is empty.⚠️';
-        }else {
-            return this.firstStack.pop();
+        }
+
+        if (this.secondStack.peek() === '⚠️The Stack is empty.⚠️ You can not use the peek ❌') {
+
+            while (this.firstStack.peek() !== '⚠️The Stack is empty.⚠️ You can not use the peek ❌') {
+
+                let value = this.firstStack.pop();
+
+                this.secondStack.push(value);
+
+            }
+
+            let values = this.secondStack.pop();
+
+            while (this.secondStack.peek() !== '⚠️The Stack is empty.⚠️ You can not use the peek ❌') {
+
+                let value = this.secondStack.pop();
+
+                this.firstStack.push(value);
+
+            }
+
+            return values;
+
         }
     }
 }
